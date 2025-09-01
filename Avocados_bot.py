372de33d73ec9cb5e8ace1e5b78cf75964a4c0fe
sakchai-01 <sakchai.te@psru.ac.th>
2025-09-01 22:56:04 +0700

    Update Avocados_bot.py

--- Avocados_bot.py
+++ Avocados_bot.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+from threading import Thread
+from flask import Flask
 import random
 import os
 
@@ -124,6 +126,19 @@ async def on_message(message):
         await message.channel.send("สวัสดีค่ะ 👋")
     await bot.process_commands(message)
 
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run():
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+keep_alive()
 bot.run(os.environ['DISCORD_TOKEN'])
-
-
